d547289ee21b78506b32312f6bd6f33878f5b825
Joe Hancuch <joseph.hancuch1@gmail.com>
2022-02-12 10:53:06 -0500

    Update app.py

--- model/app.py
+++ model/app.py
@@ -15,16 +15,20 @@ with open(model_path, "rb") as file:
 app = Flask(__name__)
 
 # use decorator to define the /score input method and define the predict function
-@app.route("/score", methods=["POST", "GET"])
-def predict_species():
+#@app.route("/score", methods=["POST", "GET"])
+@app.route("/", methods=["POST", "GET"])
+def predict():
     # create list and append inputs
     mnist_vector = []
-    flower.append(request.args.get("mnist_vector"))
+    mnist_vector.append(request.args.get("mnist_vector"))
 
     # return the prediction
     return array2string(unpickled_rf.predict([digits]))
 
 
 # run the app
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port="5001")
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", debug=True, port="5001")
+
+app.run()
+#app.run(host="0.0.0.0", debug=True, port="5001")
